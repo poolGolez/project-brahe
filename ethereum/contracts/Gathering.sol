b030f52d2f1a1a041f718cd68e3951a8ba106e6e
pool <loop.edward@gmail.com>
2022-01-19 05:40:36 +0800

    Create initial smart contract: Gathering

---
+++ ethereum/contracts/Gathering.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.11;
+
+contract Gathering {
+
+    address public manager;
+    string public name;
+    uint public downpayment;
+
+    mapping(address => bool) participants;
+    uint public participantsCount;
+
+    constructor(string memory _name, uint _downpayment) {
+        manager = msg.sender;
+        name = _name;
+        downpayment = _downpayment;
+    }
+
+    function join() public payable {
+        require(
+            msg.value >= downpayment,
+            "Insufficient funds to make a downpayment."
+        );
+
+        require(
+            !participants[msg.sender],
+            "Already registered to the gathering."
+        );
+
+        participants[msg.sender] = true;
+        participantsCount++;
+    }
+}
