99c6cce13bf1331f988412bd15df7e48aacdfa2b
pool <loop.edward@gmail.com>
2022-01-24 21:22:11 +0800

    Create Hello World application

---
+++ pages/index.js
@@ -0,0 +1,10 @@
+import { Component } from "react";
+
+class EventsListPage extends Component {
+
+    render() {
+        return (<h1>Hello, React!</h1>);
+    }
+}
+
+export default EventsListPage;
