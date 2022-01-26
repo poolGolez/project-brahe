6f286077415d1f40b0508143a455a4cdbb3fb5ad
pool <loop.edward@gmail.com>
2022-01-26 21:35:11 +0800

    Parameterize balance on GatheringCard

--- components/GatheringCard.js
+++ components/GatheringCard.js
@@ -11,11 +11,11 @@ class GatheringCard extends Component {
                     <Card.Content>
                         <Card.Header>{this.props.header}</Card.Header>
                         <Card.Meta>
-                            <Icon name='dollar' /> 0.84 eth
+                            <Icon name='dollar' />{ this.props.balance } eth
                         </Card.Meta>
                         <Container textAlign='right'>
                             <Statistic>
-                                <Statistic.Value>72</Statistic.Value>
+                                <Statistic.Value>{this.props.participantsCount}</Statistic.Value>
                                 <Statistic.Label>Registrars</Statistic.Label>
                             </Statistic>
                         </Container>
